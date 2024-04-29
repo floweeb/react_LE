f22bd2f4fe9a4b010b08fc4c4d34e496485c848e
floweeb <floydqaranja@gmail.com>
2024-04-29 23:01:46 +0300

    1st commit, hello world

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+// import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <div className='App'>
+      <s>hello world</s>
+    </div>
+  )
+}
+
+export default App
