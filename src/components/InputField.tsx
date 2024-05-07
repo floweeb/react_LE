22f54e528eef1784e7f09509368dbc7a1e61c1f7
floweeb <floydqaranja@gmail.com>
2024-05-07 16:00:08 +0300

    getting the todo and saving in todos. plus stopping the page from refreshing

--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -1,14 +1,20 @@
-// import React from "react";
+import React, { useRef } from "react";
 import './styles.css'
 
 interface Props{
     todo: string
     setTodo: React.Dispatch<React.SetStateAction<string>>
+    handleAdd: (e: React.FormEvent) => void
 }
-const InputField: React.FC<Props> = ({todo, setTodo}: Props)  => {
+const InputField: React.FC<Props> = ({todo, setTodo, handleAdd}: Props)  => {
+    const inputRef = useRef<HTMLInputElement>(null);
+    
     return(
-        <form className="input">
+        <form className="input" onSubmit={(e) => {
+            handleAdd(e)
+        }}>
             <input
+            ref={inputRef}
              type="input"
              value={todo} 
              onChange={(e)=>setTodo(e.target.value)}
