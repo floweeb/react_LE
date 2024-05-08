5ac8f58a800ea77805ae6baa1e36a7b9389207e0
floweeb <floydqaranja@gmail.com>
2024-05-08 15:13:14 +0300

    In the middle of implementing the edit feature

--- src/components/SingleTodo.tsx
+++ src/components/SingleTodo.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Todo } from '../model'
 import { AiFillDelete, AiFillEdit } from 'react-icons/ai'
 import { MdDone } from 'react-icons/md'
@@ -11,6 +11,8 @@ type Props = {
 }
 
 const SingleTodo = ({todo, todos, setTodos}: Props) => {
+  const [edit, setEdit] = useState<Boolean>(false)
+  const [editTodo, setEditTodo] = useState<string>(todo.todo)
 
   const handleDone = (id: number) => {
     setTodos(
@@ -27,14 +29,26 @@ const SingleTodo = ({todo, todos, setTodos}: Props) => {
   return (
     <form className='todos__single'>
       {
+        edit ? ():
+        (
+          
+        )
+      }
+      {
         todo.isDone ? 
         (<s className="todos__single--text">{todo.todo}</s>):
         (<span className="todos__single--text">{todo.todo}</span>)
       }
       
       <div>
-        <span className="icon"> <AiFillEdit/> </span>
-        <span className="icon"onClick={() => handleDelete(todo.id)}> 
+        <span className="icon" onClick={() =>{
+          if(!edit && !todo.isDone){
+            setEdit(!edit)
+          }}
+        }> 
+          <AiFillEdit/> 
+        </span>
+        <span className="icon" onClick={() => handleDelete(todo.id)}> 
           <AiFillDelete/> 
         </span>
         <span className="icon" onClick={() => handleDone(todo.id)}> 
