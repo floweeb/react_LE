2f1524f88c10081e35de0f8ff6fe4122a7bfb0fc
floweeb <floydqaranja@gmail.com>
2024-05-08 15:01:24 +0300

    Put in the delete and done functionality, the easier ones to do

--- src/components/SingleTodo.tsx
+++ src/components/SingleTodo.tsx
@@ -9,14 +9,37 @@ type Props = {
   todos: Todo[],
   setTodos: React.Dispatch<React.SetStateAction<Todo[]>>
 }
+
 const SingleTodo = ({todo, todos, setTodos}: Props) => {
+
+  const handleDone = (id: number) => {
+    setTodos(
+      todos.map((todo) => todo.id === id ? {
+        ...todo, isDone: !todo.isDone
+      } : todo)
+    )
+  };
+
+  const handleDelete = (id: number) => {
+    setTodos(todos.filter((todo) => todo.id !== id))
+  };
+
   return (
     <form className='todos__single'>
-      <span className="todos__single--text">{todo.todo}</span>
+      {
+        todo.isDone ? 
+        (<s className="todos__single--text">{todo.todo}</s>):
+        (<span className="todos__single--text">{todo.todo}</span>)
+      }
+      
       <div>
         <span className="icon"> <AiFillEdit/> </span>
-        <span className="icon"> <AiFillDelete/> </span>
-        <span className="icon"> <MdDone/> </span>
+        <span className="icon"onClick={() => handleDelete(todo.id)}> 
+          <AiFillDelete/> 
+        </span>
+        <span className="icon" onClick={() => handleDone(todo.id)}> 
+          <MdDone/> 
+        </span>
       </div>
     </form>
   )
